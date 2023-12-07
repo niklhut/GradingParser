10b35c7f1f9537ec17707ec350154fa26a0b9cc0
niklhut <49069026+niklhut@users.noreply.github.com>
2023-12-07 11:29:19 +0100

    Improve options description

--- src/user_interface.py
+++ src/user_interface.py
@@ -2,9 +2,11 @@ from colorama import Fore, Style
 
 def get_input_options_description():
     return (
-        f"{Fore.GREEN}Y{Style.RESET_ALL} for 'valid', "
-        f"{Fore.RED}N{Style.RESET_ALL} for 'not valid', "
-        f"{Fore.CYAN}S{Style.RESET_ALL} to skip, "
-        f"{Fore.YELLOW}B{Style.RESET_ALL} to go back | "
-        f"Press '{Fore.CYAN}Q{Style.RESET_ALL}' to quit"
+        "Press "
+        f"'{Fore.GREEN}Y{Style.RESET_ALL}' for 'valid', "
+        f"'{Fore.RED}N{Style.RESET_ALL}' for 'not valid', "
+        f"'{Fore.CYAN}S{Style.RESET_ALL}' to skip, "
+        f"'{Fore.CYAN}B{Style.RESET_ALL}' to go back "
+        f"'{Fore.CYAN}Q{Style.RESET_ALL}' to quit | "
+        f"Press '{Fore.CYAN}Enter{Style.RESET_ALL}' to confirm."
     )
