f4e1c66e313978490f2628a751d7b2a90e982457
niklhut <49069026+niklhut@users.noreply.github.com>
2023-12-08 18:56:37 +0100

    Remove unused imports

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
-import pandas as pd
-
 def save_dataframe(df, file_path):
+
+    # Save the modified DataFrame back to the CSV file
     if file_path.endswith('.xlsx'):
         df.to_excel(file_path, index=False)
     elif file_path.endswith('.csv'):
