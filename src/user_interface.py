26c5f41bf2763318bec49e4de0ba9a2182b80d41
niklhut <49069026+niklhut@users.noreply.github.com>
2024-11-23 10:26:43 +0100

    Fix description

--- src/user_interface.py
+++ src/user_interface.py
@@ -3,8 +3,8 @@ from colorama import Fore, Style
 def get_input_options_description():
     return (
         "Press "
-        f"'{Fore.GREEN}Y{Style.RESET_ALL}' for 'valid', "
-        f"'{Fore.RED}N{Style.RESET_ALL}' for 'not valid', "
+        f"'{Fore.GREEN}Y{Style.RESET_ALL}' or '{Fore.GREEN}1{Style.RESET_ALL}' for 'valid', "
+        f"'{Fore.RED}N{Style.RESET_ALL}' or '{Fore.RED}0{Style.RESET_ALL}' for 'invalid', "
         f"'{Fore.CYAN}S{Style.RESET_ALL}' to skip, "
         f"'{Fore.CYAN}B{Style.RESET_ALL}' to go back "
         f"'{Fore.CYAN}Q{Style.RESET_ALL}' to quit | "
